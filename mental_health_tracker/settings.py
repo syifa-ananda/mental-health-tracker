916d8e2ddcfdc20d3c26679d75dee3d9739ced71
Syifa Ananda <syifaananda@Syifas-MacBook-Pro.local>
2024-09-18 11:18:27 +0700

    finish

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -126,4 +126,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://<syifa-ananda31-mentalhealthtracker.pbp.cs.ui.ac.i", "https://syifa-ananda31-mentalhealthtracker.pbp.cs.ui.ac.id"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://syifa-ananda31-mentalhealthtracker.pbp.cs.ui.ac.i", "https://syifa-ananda31-mentalhealthtracker.pbp.cs.ui.ac.id"]
