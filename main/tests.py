0dde53d6a26b6cebe6d1b6021f3945d5ed18c3e9
Syifa Ananda <syifaananda@Syifas-MacBook-Pro.local>
2024-09-20 23:30:27 +0700

    quiz no.6

--- main/tests.py
+++ main/tests.py
@@ -1,23 +1,32 @@
 from django.test import TestCase, Client
 from django.utils import timezone
 from .models import MoodEntry
+from django.contrib.auth.models import User
+
 
 class MainTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='user', password='123user') 
+        self.client = Client() 
+        self.client.login(username='user', password='123user') 
+        self.client.cookies['last_login'] = '2024-09-20T12:34:56' 
+
     def test_main_url_is_exist(self):
-        response = Client().get('')
+        response = self.client.get('')
         self.assertEqual(response.status_code, 200)
 
     def test_main_using_main_template(self):
-        response = Client().get('')
+        response = self.client.get('')
         self.assertTemplateUsed(response, 'main.html')
 
     def test_nonexistent_page(self):
-        response = Client().get('/skibidi/')
+        response = self.client.get('/skibidi/')  # Non-existent URL
         self.assertEqual(response.status_code, 404)
 
     def test_strong_mood_user(self):
         now = timezone.now()
         mood = MoodEntry.objects.create(
+          user = self.user,
           mood="Happy",
           time = now,
           feelings = "I'm happy, even though my clothes are soaked from the rain :(",
@@ -26,6 +35,6 @@ class MainTest(TestCase):
         self.assertTrue(mood.is_mood_strong)
 
     def test_main_template_uses_correct_page_title(self):
-        response = Client().get("/")
+        response = self.client.get("/")
         html_response = response.content.decode("utf8")
         self.assertIn("PBD Mental Health Tracker", html_response)
